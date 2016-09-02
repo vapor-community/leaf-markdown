69f623ce7d3021461ebeee94fcb6f26343bb1c59
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-09-02 09:36:26 +0100

    Vapor 0.17

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     	Target(name: "vapor-markdown-example", dependencies: ["vapor-markdown"])
     ],
     dependencies: [
-    	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 0),
+    	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 17),
     	.Package(url: "https://github.com/czechboy0/cmark.swift.git", majorVersion: 0, minor: 1)
     ]
 )
