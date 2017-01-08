e5fc6f68579e38086f9789a3dc372194f70128a3
Tim <0xtimc@gmail.com>
2017-01-08 13:02:24 +0000

    Remove old example target from Package.swift

--- Package.swift
+++ Package.swift
@@ -2,9 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "VaporMarkdown",
-    targets: [
-    	Target(name: "vapor-markdown-example", dependencies: ["VaporMarkdown"])
-    ],
     dependencies: [
     	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
     	.Package(url: "https://github.com/czechboy0/cmark.swift.git", majorVersion: 0, minor: 1)
