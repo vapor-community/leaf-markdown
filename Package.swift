50b88a2e2cbe1ea36455ce5f1273d94ae05c23db
Tim <0xtimc@gmail.com>
2017-01-09 19:15:26 +0000

    Switch to the BrokenHands branch of SwiftMarkdown to remove compiler warning

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "LeafMarkdown",
     dependencies: [
     	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
-    	.Package(url: "https://github.com/czechboy0/cmark.swift.git", majorVersion: 0, minor: 1)
+    	.Package(url: "https://github.com/brokenhandsio/SwiftMarkdown.git", majorVersion: 0, minor: 1)
     ]
 )
