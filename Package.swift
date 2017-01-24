90797aa528991fb1b13fca2847d40e0f03075f00
Tim Condon <tim.condon@bbc.co.uk>
2017-01-24 12:39:26 +0000

    Update Vapor to use 1.4

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "LeafMarkdown",
     dependencies: [
-    	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
+    	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 4),
     	.Package(url: "https://github.com/brokenhandsio/SwiftMarkdown.git", majorVersion: 0, minor: 1)
     ]
 )
