82f8572ea5914f638810a5adbc0c837258894248
Tim <0xtimc@gmail.com>
2018-01-02 15:24:26 +0000

    Port tag code to Vapor 3

--- Sources/MarkdownProvider/Tag.swift
+++ Sources/MarkdownProvider/Tag.swift
@@ -4,41 +4,28 @@ import Async
 
 public final class Markdown: LeafTag {
 
+    public enum Error: Swift.Error {
+        case invalidArgument(LeafData?)
+    }
+
     public let name = "markdown"
 
     public func render(parsed: ParsedTag, context: LeafContext, renderer: LeafRenderer) throws -> Future<LeafData?> {
-        return Future(nil)
+
+        var markdown = ""
+
+        if let markdownArgument = parsed.parameters.first, !markdownArgument.isNull {
+            guard let markdownArgumentValue = markdownArgument.string else {
+                throw Error.invalidArgument(parsed.parameters.first)
+            }
+            markdown = markdownArgumentValue
+        }
+
+        let markdownHtml = try markdownToHTML(markdown)
+
+        let promise = Promise(LeafData?.self)
+        promise.complete(.string(markdownHtml))
+        return promise.future
     }
-
-
-
-
-
-//    public enum Error: Swift.Error {
-//        case invalidArgument(Argument?)
-//    }
-//
-//    public init() { }
-//
-//    public let name = "markdown"
-//
-//    public func shouldRender(stem: Stem, context: Context, tagTemplate: TagTemplate, arguments: [Argument], value: Node?) -> Bool{
-//        return true
-//    }
-//
-//    public func run(tagTemplate: TagTemplate, arguments: ArgumentList) throws -> Node? {
-//        var markdown = ""
-//
-//        if let markdownArgument = arguments.first {
-//            guard let markdownArgumentValue = markdownArgument.string else {
-//                throw Error.invalidArgument(arguments.list.first)
-//            }
-//            markdown = markdownArgumentValue
-//        }
-//
-//        let markdownHtml = try markdownToHTML(markdown)
-//        let unescaped = markdownHtml.bytes
-//        return .bytes(unescaped)
-//    }
 
 }
--- Tests/MarkdownProviderTests/LeafTests.swift
+++ Tests/MarkdownProviderTests/LeafTests.swift
@@ -19,30 +19,20 @@ class LeafTests: XCTestCase {
         self.renderer = LeafRenderer(config: LeafConfig(tags: [tag.name: tag]), on: queue)
     }
     
-    func testRunTag() {
+    func testRunTag() throws {
         let inputMarkdown = "# This is a test\n\nWe have some text in a tag"
         let data = LeafData.dictionary(["data": .string(inputMarkdown)])
         let expectedHtml = "<h1>This is a test</h1>\n<p>We have some text in a tag</p>\n"
 
-        do {
-            let result = try renderer.render(template, context: LeafContext(data: data)).blockingAwait()
-            XCTAssertEqual(result, expectedHtml)
-        }
-        catch {
-            XCTFail()
-        }
+        let result = try renderer.render(template, context: LeafContext(data: data)).blockingAwait()
+        XCTAssertEqual(result, expectedHtml)
     }
     
-    func testNilParameterDoesNotCrashLeaf() {
+    func testNilParameterDoesNotCrashLeaf() throws {
         let data = LeafData.dictionary(["data": .null])
         let expectedHtml = ""
 
-        do {
-            let result = try renderer.render(template, context: LeafContext(data: data)).blockingAwait()
-            XCTAssertEqual(result, expectedHtml)
-        }
-        catch {
-            XCTFail()
-        }
+        let result = try renderer.render(template, context: LeafContext(data: data)).blockingAwait()
+        XCTAssertEqual(result, expectedHtml)
     }
 }
