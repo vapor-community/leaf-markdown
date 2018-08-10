6440f2ef329065ba9a6af8d3fbd3e6cde086b56d
Tim <0xtimc@gmail.com>
2018-08-10 19:31:39 +0100

    Use Leaf release

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "LeafMarkdown", targets: ["LeafMarkdown"]),
     ],
     dependencies: [
-    	.package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
+    	.package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor-community/markdown.git", .upToNextMajor(from: "0.4.0")),
     ],
     targets: [
