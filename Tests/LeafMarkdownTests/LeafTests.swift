4da1b61336035abb47a24f5becc519b697c21837
Tim <0xtimc@gmail.com>
2019-12-02 13:05:09 +0000

    Only use the dependencies we need

--- Tests/LeafMarkdownTests/LeafTests.swift
+++ Tests/LeafMarkdownTests/LeafTests.swift
@@ -1,29 +1,26 @@
 import XCTest
-import Vapor
-
 @testable import Leaf
 import LeafMarkdown
 
 class LeafTests: XCTestCase {
-    static var allTests = [
-        ("testRunTag", testRunTag),
-        ("testNilParameterDoesNotCrashLeaf", testNilParameterDoesNotCrashLeaf),
-        ("testStripHtml", testStripHtml),
-        ("testDoNotStripHtml", testDoNotStripHtml)
-    ]
 
+    // MARK: - Properties
+    
     var renderer: LeafRenderer!
     let template = "#markdown(data)"
 
+    // MARK: - Overrides
+    
     override func setUp() {
         let queue = EmbeddedEventLoop()
         let container = BasicContainer(config: .init(), environment: .testing, services: .init(), on: queue)
         let tag = Markdown()
         var leafTagConfig = LeafTagConfig.default()
         leafTagConfig.use(tag, as: tag.name)
-        self.renderer = LeafRenderer(config: LeafConfig(tags: leafTagConfig, viewsDir: "", shouldCache: false),
-                                     using: container)
+        self.renderer = LeafRenderer(config: LeafConfig(tags: leafTagConfig, viewsDir: "", shouldCache: false), using: container)
     }
+    
+    // MARK: - Tests
 
     func testRunTag() throws {
         let inputMarkdown = "# This is a test\n\nWe have some text in a tag"
@@ -72,6 +69,5 @@ class LeafTests: XCTestCase {
         let result = try renderer.render(template: template.data(using: .utf8)!, data).wait()
         let resultString = String(data: result.data, encoding: .utf8)!
         XCTAssertEqual(resultString, expectedHtml)
-
     }
 }
