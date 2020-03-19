34cbdad1e92c6d6db39a40c88cb74b109065d152
Joannis Orlandos <joannis@orlandos.nl>
2020-03-19 14:00:39 +0100

    Leaf 4 support

--- Package.swift
+++ Package.swift
@@ -4,16 +4,18 @@ import PackageDescription
 
 let package = Package(
     name: "LeafMarkdown",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .library(name: "LeafMarkdown", targets: ["LeafMarkdown"]),
     ],
     dependencies: [
-    	.package(url: "https://github.com/vapor/template-kit.git", from: "1.4.0"),
         .package(url: "https://github.com/vapor-community/markdown.git", from: "0.6.1"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.0.0"),
     ],
     targets: [
-        .target(name: "LeafMarkdown", dependencies: ["TemplateKit", "SwiftMarkdown"]),
-        .testTarget(name: "LeafMarkdownTests", dependencies: ["LeafMarkdown", "Leaf"]),
+        .target(name: "LeafMarkdown", dependencies: ["LeafKit", "SwiftMarkdown"]),
+        .testTarget(name: "LeafMarkdownTests", dependencies: ["LeafMarkdown", "LeafKit"]),
     ]
 )
--- Tests/LeafMarkdownTests/LeafTests.swift
+++ Tests/LeafMarkdownTests/LeafTests.swift
@@ -1,73 +1,85 @@
 import XCTest
-@testable import Leaf
+@testable import LeafKit
 import LeafMarkdown
 
-class LeafTests: XCTestCase {
-
+class MarkdownTests: XCTestCase {
     // MARK: - Properties
     
     var renderer: LeafRenderer!
-    let template = "#markdown(data)"
+    var template: ResolvedDocument!
 
     // MARK: - Overrides
     
     override func setUp() {
-        let queue = EmbeddedEventLoop()
-        let container = BasicContainer(config: .init(), environment: .testing, services: .init(), on: queue)
-        let tag = Markdown()
-        var leafTagConfig = LeafTagConfig.default()
-        leafTagConfig.use(tag, as: tag.name)
-        self.renderer = LeafRenderer(config: LeafConfig(tags: leafTagConfig, viewsDir: "", shouldCache: false), using: container)
+        let loop = EmbeddedEventLoop()
+        let config = LeafConfiguration(rootDirectory: Process().currentDirectoryPath)
+        var tags = defaultTags
+        tags["markdown"] = Markdown()
+        self.renderer = LeafRenderer(
+            configuration: config,
+            tags: tags,
+            files: NIOLeafFiles(fileio: .init(threadPool: .init(numberOfThreads: 1))),
+            eventLoop: loop
+        )
+        
+        do {
+            var lexer = LeafLexer(name: "markdowntest", template: "#markdown(data)")
+            let tokens = try lexer.lex()
+            var parser = LeafParser(name: "markdowntest", tokens: tokens)
+            let syntax = try parser.parse()
+            let unresolved = UnresolvedDocument(name: "markdowntest", raw: syntax)
+            let resolver = ExtendResolver(document: unresolved, dependencies: [])
+            template = try resolver.resolve(rootDirectory: config.rootDirectory)
+        } catch {
+            XCTFail()
+        }
     }
     
     // MARK: - Tests
 
     func testRunTag() throws {
         let inputMarkdown = "# This is a test\n\nWe have some text in a tag"
-        let data = TemplateData.dictionary(["data": .string(inputMarkdown)])
         let expectedHtml = "<h1>This is a test</h1>\n<p>We have some text in a tag</p>\n"
 
-        let result = try renderer.render(template: template.data(using: .utf8)!, data).wait()
-        let resultString = String(data: result.data, encoding: .utf8)!
+        let result = try renderer.render(template, context: ["data": .string(inputMarkdown)])
+        let resultString = result.getString(at: 0, length: result.readableBytes)
         XCTAssertEqual(resultString, expectedHtml)
     }
 
     func testNilParameterDoesNotCrashLeaf() throws {
-        let data = TemplateData.dictionary(["data": .null])
         let expectedHtml = ""
-
-        let result = try renderer.render(template: template.data(using: .utf8)!, data).wait()
-        let resultString = String(data: result.data, encoding: .utf8)!
+        let result = try renderer.render(template, context: ["data": .null])
+        let resultString = result.getString(at: 0, length: result.readableBytes)
         XCTAssertEqual(resultString, expectedHtml)
     }
 
     func testStripHtml() throws {
         let inputMarkdown = "<br>"
-        let data = TemplateData.dictionary(["data": .string(inputMarkdown)])
         let expectedHtml = "<!-- raw HTML omitted -->\n"
 
-        let result = try renderer.render(template: template.data(using: .utf8)!, data).wait()
-        let resultString = String(data: result.data, encoding: .utf8)!
+        let result = try renderer.render(template, context: ["data": .string(inputMarkdown)])
+        let resultString = result.getString(at: 0, length: result.readableBytes)
         XCTAssertEqual(resultString, expectedHtml)
 
     }
 
     func testDoNotStripHtml() throws {
-
-        let queue = EmbeddedEventLoop()
-        let container = BasicContainer(config: .init(), environment: .testing, services: .init(), on: queue)
-        let tag = Markdown(options: [.unsafe])
-        var leafTagConfig = LeafTagConfig.default()
-        leafTagConfig.use(tag, as: tag.name)
-        let renderer = LeafRenderer(config: LeafConfig(tags: leafTagConfig, viewsDir: "", shouldCache: false),
-                                     using: container)
+        let loop = EmbeddedEventLoop()
+        let config = LeafConfiguration(rootDirectory: Process().currentDirectoryPath)
+        var tags = defaultTags
+        tags["markdown"] = Markdown(options: [])
+        let renderer = LeafRenderer(
+            configuration: config,
+            tags: tags,
+            files: NIOLeafFiles(fileio: .init(threadPool: .init(numberOfThreads: 1))),
+            eventLoop: loop
+        )
 
         let inputMarkdown = "<br>"
-        let data = TemplateData.dictionary(["data": .string(inputMarkdown)])
         let expectedHtml = "<br>\n"
 
-        let result = try renderer.render(template: template.data(using: .utf8)!, data).wait()
-        let resultString = String(data: result.data, encoding: .utf8)!
+        let result = try renderer.render(template, context: ["data": .string(inputMarkdown)])
+        let resultString = result.getString(at: 0, length: result.readableBytes)
         XCTAssertEqual(resultString, expectedHtml)
     }
 }
