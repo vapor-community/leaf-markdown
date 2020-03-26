84aab98e03a38bb0d993b131b597b21e892d3979
Joannis Orlandos <joannis@orlandos.nl>
2020-03-26 13:46:45 +0100

    Add failure test

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .target(name: "LeafMarkdown", dependencies: [
             .product(name: "LeafKit", package: "leaf-kit"),
-            .product(name: "SwiftMarkdown", package: "markdown"),
+            .product(name: "SwiftMarkdown", package: "SwiftMarkdown"),
         ]),
         .testTarget(name: "LeafMarkdownTests", dependencies: [
             .target(name: "LeafMarkdown"),
