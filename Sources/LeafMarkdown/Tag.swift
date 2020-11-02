f74578c100427bbeb5867b869d7c0a107a61d132
Tim <0xtimc@gmail.com>
2020-11-02 14:52:04 +0000

    Update Tag to Tau release

--- Sources/LeafMarkdown/Tag.swift
+++ Sources/LeafMarkdown/Tag.swift
@@ -1,32 +1,35 @@
 import LeafKit
 import SwiftMarkdown
 
-public struct Markdown: LeafTag {
-    public enum Error: Swift.Error {
-        case invalidArgument(LeafData?)
-    }
+public struct Markdown: LeafFunction, StringReturn, Invariant {
     
     private let options: MarkdownOptions?
     
     public init(options: MarkdownOptions? = nil) {
         self.options = options
     }
-    
-    public func render(_ ctx: LeafContext) throws -> LeafData {
+
+    public static let callSignature: [LeafCallParameter] = [.string]
+
+    public func evaluate(_ params: LeafCallValues) -> LeafData {
         var markdown = ""
-        
-        if let markdownArgument = ctx.parameters.first, !markdownArgument.isNull {
-            guard let markdownArgumentValue = markdownArgument.string else {
-                throw Error.invalidArgument(ctx.parameters.first)
+
+        if params.count > 0 {
+            guard let markdownArgumentValue = params[0].string else {
+                return .error("Failed to get markdown from parameter")
             }
             markdown = markdownArgumentValue
         }
 
         let markdownHTML: String
-        if let options = options {
-            markdownHTML = try markdownToHTML(markdown, options: options)
-        } else {
-            markdownHTML = try markdownToHTML(markdown)
+        do {
+            if let options = options {
+                markdownHTML = try markdownToHTML(markdown, options: options)
+            } else {
+                markdownHTML = try markdownToHTML(markdown)
+            }
+        } catch {
+            return .error("Failed to convert markdown string to HTML - \(error)")
         }
 
         return .string(markdownHTML)
