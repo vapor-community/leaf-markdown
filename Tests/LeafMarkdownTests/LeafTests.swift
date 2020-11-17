a55f93747cc7dd1fab666a04ed571bcd2c856401
Tim <0xtimc@gmail.com>
2020-11-17 08:21:21 +0000

    Revert "Get the tests working"
    This reverts commit dd33180fc4dedaad29dd10f39274466a32f674f3.

--- Tests/LeafMarkdownTests/LeafTests.swift
+++ Tests/LeafMarkdownTests/LeafTests.swift
@@ -7,17 +7,15 @@ class MarkdownTests: XCTestCase {
     
     var renderer: LeafRenderer!
     var eventLoop: EventLoop!
-    var leafSource: StubbedLeafSource!
+//    var template: ResolvedDocument!
     let templateString = "#markdown(data)"
     let template = "template"
 
     // MARK: - Overrides
     override func setUp() {
         eventLoop = EmbeddedEventLoop()
-        LeafConfiguration.started = false
-        LeafConfiguration.entities = .leaf4Core
 
-        leafSource = StubbedLeafSource(templateName: template, templateContents: templateString)
+        let leafSource = StubbedLeafSource(templateName: template, templateContents: templateString)
         LeafConfiguration.entities.use(Markdown(), asFunction: "markdown")
         self.renderer = LeafRenderer(cache: DefaultLeafCache(), sources: .singleSource(leafSource), eventLoop: eventLoop)
     }
@@ -32,37 +30,53 @@ class MarkdownTests: XCTestCase {
         let inputMarkdown = "# This is a test\n\nWe have some text in a tag"
         let expectedHtml = "<h1>This is a test</h1>\n<p>We have some text in a tag</p>\n"
 
-        let resultString = try getRenderedString(input: .string(inputMarkdown))
+        let resultString = try getRenderedString(input: inputMarkdown)
         XCTAssertEqual(resultString, expectedHtml)
     }
 
-    func testStripHtml() throws {
-        let inputMarkdown = "<br>"
-        let expectedHtml = "<!-- raw HTML omitted -->\n"
+//    func testNilParameterDoesNotCrashLeaf() throws {
+//        let expectedHtml = ""
+//        let result = try renderer.render(template, context: ["data": .null])
+//        let resultString = result.getString(at: 0, length: result.readableBytes)
+//        XCTAssertEqual(resultString, expectedHtml)
+//    }
+//
+//    func testStripHtml() throws {
+//        let inputMarkdown = "<br>"
+//        let expectedHtml = "<!-- raw HTML omitted -->\n"
+//
+//        let result = try renderer.render(template, context: ["data": .string(inputMarkdown)])
+//        let resultString = result.getString(at: 0, length: result.readableBytes)
+//        XCTAssertEqual(resultString, expectedHtml)
+//    }
+//
+//    func testRejectBadData() throws {
+//        let data = LeafData.lazy { .null }
+//        XCTAssertThrowsError(try renderer.render(template, context: ["data": data]))
+//    }
+//
+//    func testDoNotStripHtml() throws {
+//        let loop = EmbeddedEventLoop()
+//        let config = LeafConfiguration(rootDirectory: Process().currentDirectoryPath)
+//        var tags = defaultTags
+//        tags["markdown"] = Markdown(options: [.unsafe])
+//        let renderer = LeafRenderer(
+//            configuration: config,
+//            tags: tags,
+//            files: NIOLeafFiles(fileio: .init(threadPool: .init(numberOfThreads: 1))),
+//            eventLoop: loop
+//        )
+//
+//        let inputMarkdown = "<br>"
+//        let expectedHtml = "<br>\n"
+//
+//        let result = try renderer.render(template, context: ["data": .string(inputMarkdown)])
+//        let resultString = result.getString(at: 0, length: result.readableBytes)
+//        XCTAssertEqual(resultString, expectedHtml)
+//    }
 
-        let resultString = try getRenderedString(input: .string(inputMarkdown))
-        XCTAssertEqual(resultString, expectedHtml)
-    }
-
-    func testRejectBadData() throws {
-        XCTAssertThrowsError(try getRenderedString(input: .init(nilLiteral: ())))
-    }
-
-    func testDoNotStripHtml() throws {
-        LeafConfiguration.started = false
-        LeafConfiguration.entities = .leaf4Core
-        LeafConfiguration.entities.use(Markdown(options: .unsafe), asFunction: "markdown")
-        self.renderer = LeafRenderer(cache: DefaultLeafCache(), sources: .singleSource(leafSource), eventLoop: eventLoop)
-
-        let inputMarkdown = "<br>"
-        let expectedHtml = "<br>\n"
-
-        let resultString = try getRenderedString(input: .string(inputMarkdown))
-        XCTAssertEqual(resultString, expectedHtml)
-    }
-
-    private func getRenderedString(input: LeafData) throws -> String {
-        let result = try renderer.render(template: template, context: ["data": input]).wait()
+    private func getRenderedString(input: String) throws -> String {
+        let result = try renderer.render(template: template, context: ["data": .string(input)]).wait()
         let resultString = String(decoding: result.readableBytesView, as: UTF8.self)
         return resultString
     }
